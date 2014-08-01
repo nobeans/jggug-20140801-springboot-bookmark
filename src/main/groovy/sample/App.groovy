477be0d764c09c9bc0ea00a95f28ed21fc2866be
Yasuharu Nakano <ynak@jggug.org>
2014-08-01 16:07:54 +0900

    Fix the inject error by `spring.aop.proxyTargetClass = true` configuration

--- src/main/groovy/sample/App.groovy
+++ src/main/groovy/sample/App.groovy
@@ -2,14 +2,8 @@ package sample
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.AdviceMode
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.transaction.annotation.EnableTransactionManagement
 
-// To avoid an error of DI caused by using transactional proxy.
-// http://stackoverflow.com/questions/18369258/spring-aop-at-service-layer
-// http://docs.spring.io/spring/docs/4.0.x/spring-framework-reference/html/transaction.html
-@EnableTransactionManagement(mode = AdviceMode.ASPECTJ)
 @EnableAutoConfiguration
 @ComponentScan
 class App {
