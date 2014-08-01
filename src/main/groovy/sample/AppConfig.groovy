fd9a86e9a93e9ff9a7c9ff19f641024a9b5eb6d7
Yasuharu Nakano <ynak@jggug.org>
2014-08-01 17:17:57 +0900

    Log SQL statement by log4jdbc-remix

---
+++ src/main/groovy/sample/AppConfig.groovy
@@ -0,0 +1,36 @@
+package sample
+
+import net.sf.log4jdbc.Log4jdbcProxyDataSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+import javax.sql.DataSource
+
+@Configuration
+public class AppConfig {
+
+    @Autowired
+    DataSourceProperties properties
+    DataSource dataSource
+
+    @ConfigurationProperties(prefix = DataSourceAutoConfiguration.CONFIGURATION_PREFIX)
+    @Bean(destroyMethod = "close")
+    DataSource realDataSource() {
+        def builder = DataSourceBuilder.create(properties.classLoader)
+            .url(properties.url)
+            .username(properties.username)
+            .password(properties.password)
+        dataSource = builder.build()
+        return dataSource
+    }
+
+    @Bean
+    DataSource dataSource() {
+        new Log4jdbcProxyDataSource(dataSource)
+    }
+}
